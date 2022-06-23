d97190dadfd6b0d3a2fe754fe70b37ff6fe9173a
mahmoud947 <65209396+mahmoud947@users.noreply.github.com>
2022-06-23 08:33:12 +0200

    DateLayer: Implement note repository

---
+++ lib/features/note/data/repositories/note_repository_impl.dart
@@ -0,0 +1,60 @@
+import 'package:dartz/dartz.dart';
+import 'package:mythic_todo/core/error/exceptions.dart';
+
+import 'package:mythic_todo/core/error/failures.dart';
+import 'package:mythic_todo/features/note/data/data_sources/local/entities/note_entity.dart';
+import 'package:mythic_todo/features/note/data/data_sources/local/note_dao.dart';
+import 'package:mythic_todo/features/note/data/mapper/mapper.dart';
+import 'package:mythic_todo/features/note/domain/models/note.dart';
+import 'package:mythic_todo/features/note/domain/repositories/note_repository.dart';
+
+class NoteRepositoryImpl implements NoteRepository {
+  NoteRepositoryImpl({required this.noteDao});
+  final NoteDao noteDao;
+  @override
+  Future<Either<Failure, List<Note>>> getNotes() async {
+    try {
+      final localNotes = await noteDao.getNotes();
+      final List<Note> notes =
+          localNotes.map((localNote) => localNote.toDomain()).toList();
+      return Right(notes);
+    } on LocalDatabaseException catch (e) {
+      return Left(LocalDatabaseFailure(message: e.message));
+    }
+  }
+
+  @override
+  Future<Either<Failure, Unit>> deleteNote({required int noteId}) async {
+    try {
+      await noteDao.deleteNote(noteId: noteId);
+      return const Right(unit);
+    } on LocalDatabaseException catch (e) {
+      return Left(LocalDatabaseFailure(message: e.message));
+    }
+  }
+
+  @override
+  Future<Either<Failure, Unit>> insertNote({
+    required NoteEntity noteEntity,
+  }) async {
+    try {
+      await noteDao.insertNote(noteEntity: noteEntity);
+      return const Right(unit);
+    } on LocalDatabaseException catch (e) {
+      return Left(LocalDatabaseFailure(message: e.message));
+    }
+  }
+
+  @override
+  Future<Either<Failure, Note>> updateNote({
+    required NoteEntity noteEntity,
+  }) async {
+    try {
+      final localUpdatedResult =
+          await noteDao.updateNote(noteEntity: noteEntity);
+      return Right(localUpdatedResult.toDomain());
+    } on LocalDatabaseException catch (e) {
+      return Left(LocalDatabaseFailure(message: e.message));
+    }
+  }
+}
