95f2484f3f52743ec25ee6e1995e13d61416c233
mahmoud947 <65209396+mahmoud947@users.noreply.github.com>
2022-06-23 08:19:07 +0200

    refactor(domain): add missing function parameters to Repository

--- lib/features/note/domain/repositories/note_repository.dart
+++ lib/features/note/domain/repositories/note_repository.dart
@@ -1,10 +1,11 @@
 import 'package:dartz/dartz.dart';
 import 'package:mythic_todo/core/error/failures.dart';
+import 'package:mythic_todo/features/note/data/data_sources/local/entities/note_entity.dart';
 import 'package:mythic_todo/features/note/domain/models/note.dart';
 
 abstract class NoteRepository {
   Future<Either<Failure, List<Note>>> getNotes();
-  Future<Either<Failure, Unit>> insertNote();
-  Future<Either<Failure, Unit>> deleteNote();
-  Future<Either<Failure, Note>> updateNote();
+  Future<Either<Failure, Unit>> insertNote(NoteEntity noteEntity);
+  Future<Either<Failure, Unit>> deleteNote(int noteId);
+  Future<Either<Failure, Note>> updateNote(NoteEntity noteEntity);
 }
