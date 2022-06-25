c786f10d5c1d317b8aa8b0e1bb8f382316261119
mahmoud947 <65209396+mahmoud947@users.noreply.github.com>
2022-06-25 09:54:04 +0200

    test(note): GetNotesUseCase

---
+++ test/features/note/domain/usecases/get_note_use_case_test.dart
@@ -0,0 +1,42 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:mocktail/mocktail.dart';
+import 'package:mythic_todo/features/note/data/data_sources/local/entities/note_entity.dart';
+import 'package:mythic_todo/features/note/domain/models/note.dart';
+import 'package:mythic_todo/features/note/domain/repositories/note_repository.dart';
+import 'package:mythic_todo/features/note/domain/usecases/get_note_use_case.dart';
+
+class MockNoteRepository extends Mock implements NoteRepository {}
+
+void main() {
+  late MockNoteRepository mockNoteRepository;
+  late GetNoteUseCase useCase;
+
+  setUp(() {
+    mockNoteRepository = MockNoteRepository();
+    useCase = GetNoteUseCase(repository: mockNoteRepository);
+  });
+  const Note tNote = Note(
+      id: 1,
+      title: 'title1',
+      description: 'description',
+      startTime: 'startTime',
+      endTime: 'endTime',
+      color: NoteColor.babyBlue,
+      isCompleted: true,
+      reminder: true);
+  test('should get note from the repository', () async {
+    when(() => mockNoteRepository.getNote(noteId: 1))
+        .thenAnswer((_) async => const Right(tNote));
+    final result = await useCase(noteId: 1);
+
+    expect(result, const Right(tNote));
+
+    verify(
+      () => mockNoteRepository.getNote(noteId: 1),
+    );
+
+    verifyNoMoreInteractions(mockNoteRepository);
+  });
+}
