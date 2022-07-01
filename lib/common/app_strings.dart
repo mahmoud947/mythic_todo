f1db4bba8df73aff97cd3bd0fd6b7ad713d9a324
Mahmoud Kamal Fawzy <kokolook94@gmail.com>
2022-07-01 18:49:17 +0200

    add: AppStrings

---
+++ lib/common/app_strings.dart
@@ -0,0 +1,5 @@
+class AppStrings {
+  static const onBoardingTitleOne = 'Welcome to mythic todo';
+  static const onBoardingTitleTow = 'Strategic plan';
+  static const onBoardingTitleThree = 'Sync todo list';
+}
